6c9abd00100ca6eeea5060b501040f1dce6e8c2d
Jonathan Beal <Jon.Beal@jax.org>
2001-09-21 18:21:00 +0000

    added stripHtmlTags()

--- mgi_html.py
+++ mgi_html.py
@@ -1,5 +1,6 @@
 # mgdhtml.py
-# functions for writing standard HTML elements to sys.stdout
+# functions for writing standard HTML elements to sys.stdout, and assorted
+#	other HTML-related functions
 
 import sys
 import os
@@ -7,8 +8,10 @@ import string
 import types
 import time
 import regex
+import regsub
 import cgi
 import urllib
+import rand
 from signal import signal, alarm, SIGALRM
 
 # global variable:
@@ -228,6 +231,182 @@ def escape(html):
 	else:
 		html = ''
 	return html
+
+def stripHtmlTags (
+	source,		# string or list of strings; text to have its HTML
+			#	tags removed
+	subLF = 0	# boolean (0/1); 1 to substitue a newline character
+			#	for any <BR> or <P> tags found
+	):
+	# Purpose: strip any HTML tags out of the 'source', so that it is left
+	#	as plain text
+	# Returns: string or list of strings, whichever type was passed in
+	# Assumes: nothing
+	# Effects: nothing
+	# Throws: nothing
+
+	# delimiter = what string is used to join the items of 'source' into
+	#	a single string?  (or None, if 'source' is already a string)
+	# s = what string should we strip the HTML tags from?
+
+	if type(source) == types.ListType:
+		s, delimiter = joinUnique (source)
+	else:
+		s = source
+		delimiter = None
+
+	# if we need to replace <BR> and <P> tags with line break characters,
+	# then do so before we strip out all the tags.
+
+	if subLF:
+		s = regsub.gsub ('<[bB][rR]>', '\n', s)
+		s = regsub.gsub ('<[pP]>', '\n', s)
+
+	# finally, strip the HTML tags, leaving in place any delimiters in
+	# mid-tag.  (This could happen if a tag was split across multiple
+	# strings in 'source'.)
+
+	s = deleteTags (s, delimiter)
+
+	if delimiter:
+		return string.split (s, delimiter)
+	return s
+
+###--- Private Functions ---###
+
+def joinUnique (
+	list		# list of strings to join together
+	):
+	# Purpose: join the strings of 'list' into a single string, using
+	#	a delimiter which does not already occur in any of the
+	#	strings in 'list'
+	# Returns: two-item tuple:  (string created, string delimiter used)
+	# Assumes: nothing
+	# Effects: nothing
+	# Throws: nothing
+
+	separator = None	# string; the delimiter we'll use to join
+
+	# let's have a set of standard separators to try first...
+
+	standard_separators = [ '@@', '##', '&&', ';;', '..', '::' ]
+
+	for sep in standard_separators:
+
+		# if 'sep' occurs in one of the strings of the 'list', then
+		# break out and try the next 'standard_separator'
+
+		for item in list:
+			if string.find (item, sep) != -1:
+				break
+
+		# if this loop ended normally (without a 'break'), then we
+		# found a delimiter that will work.  Remember it and break
+		# out of the outer loop.
+
+		else:
+			separator = sep
+			break
+
+	# if we've not yet found a delimiter, then all of the standard ones
+	# occurred somewhere in the 'list'.  We'll now simply add random
+	# letters to a string until we arrive at one which doesn't occur in
+	# one of the strings of 'list'.
+
+	if not separator:
+		sep = rand.choice (string.letters)
+		while 1:
+			# we need to see if the current 'sep' occurs in a
+			# string of 'list'.
+
+			for item in list:
+
+				# if it occurs, then add a new random letter
+				# to the end and try again.
+
+				if string.find (item, sep) != -1:
+					sep = sep + \
+						rand.choice (string.letters)
+					break
+
+			# if the 'for' loop terminates normally, then we have
+			# found a delimiter we can use.  Remember it and break
+			# out of the 'while' loop.
+
+			else:
+				separator = sep
+				break
+
+	# finally, join the list into a string and return the two-item tuple
+
+	return string.join (list, separator), separator
+
+def deleteTags (
+	s,		# string from which to remove HTML tags
+	delimiter	# string used as a delimiter to join multiple other
+			#	strings together into 's'.  may be None.
+	):
+	# Purpose: remove all HTML tags from string 's'
+	# Returns: string
+	# Assumes: nothing
+	# Effects: nothing
+	# Throws: nothing
+	# Notes: if the 'delimiter' occurs in an HTML tag in 's', we should
+	#	leave the delmiter in place and just remove the rest of the
+	#	tag.  This ensures that the string may be split apart properly
+	#	by the calling routine.
+
+	t = ''		# string we're building to return
+	openTags = []	# string; stack of tags that are still open, but which
+			#	are not the most recently opened tag
+	tag = None	# string; currently open tag
+
+	# we need to go through each character 'c' in our source string 's'...
+
+	for c in s:
+		if c == '<':
+			# if we find an '<' while we're already in an open
+			# tag, then store the already-open one in 'openTags'
+			# and start collecting characters for the new 'tag'.
+
+			if tag:
+				openTags.append (tag)
+			tag = '<'
+
+		elif c == '>':
+			# if we've reached the close of a tag, then we need
+			# to strip it down to just the delimiters it contains
+			# (or an empty string if we're not using a delimiter)
+
+			if delimiter:
+				stripped = delimiter * \
+					string.count (tag, delimiter)
+			else:
+				stripped = ''
+
+			# if we've stored previously opened tags, then pop the
+			# most recent one from the stack and add 'stripped' to
+			# it.  Otherwise, we've finished the outermost tag so
+			# we can add it to 't'.
+
+			if openTags:
+				tag = openTags[-1]
+				del openTags[-1]
+				tag = tag + stripped
+			else:
+				t = t + stripped
+				tag = None
+		elif tag:
+			# we're just collecting letters for the currently
+			# open tag.
+
+			tag = tag + c
+		else:
+			# we're not in a tag; we're just collecting characters
+			# for the string we're producing -- 't'
+
+			t = t + c
+	return t
 
 #
 # Warranty Disclaimer and Copyright Notice
